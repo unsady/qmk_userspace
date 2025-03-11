7e81df9a4dd0f2e7210ecb18e7dd6e7d18c7d717
unsady <unsady@gmail.com>
2025-03-11 16:55:57 +0100

    [refactor] update keymap definitions for improved layer functionality and enable CAPS_WORD feature

--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
@@ -12,11 +12,10 @@ enum layers {
 };
 
 // Aliases for readability
-#define QWERTY DF(_QWERTY)
-
-#define SYM MO(_SYM)
+#define QWERTY TO(_QWERTY)
+#define SYM OSL(_SYM)
 #define NAV TO(_NAV)
-#define FKEYS MO(_FUNCTION)
+#define FKEYS OSL(_FUNCTION)
 #define ADJUST MO(_ADJUST)
 
 #define GUI_ESC MT(MOD_LGUI, KC_ESC)
@@ -62,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_GRV  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_MINS,
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      GUI_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,GUI_QUOT,
-     ONE_SFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, RSFT_EQL,
+     ONE_SFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,CW_TOGG,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, RSFT_EQL,
                                 ADJUST , KC_LCTL, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RCTL, KC_APP,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
@@ -88,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_NAV] = LAYOUT_elora_hlc(
       _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______, 
-      _______, _______, _______, _______, G(KC_SPC), _______,                                     _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, 
+      _______, QWERTY , _______, _______, G(KC_SPC), _______,                                   _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, 
       _______, KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______,                                     _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
       _______, _______, _______, _______, _______, _______, _______, KC_SCRL, _______, _______, _______, KC_TAB , KC_ENT,  KC_BSPC,  KC_ESC , _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/rules.mk
@@ -1,2 +1,3 @@
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
+CAPS_WORD_ENABLE = yes
