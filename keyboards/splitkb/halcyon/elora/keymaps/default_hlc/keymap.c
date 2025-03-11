6076a3d2bf2b5d15cd5c16c2f93ecc4eb2ea2a5d
unsady <unsady@gmail.com>
2025-03-11 17:18:24 +0100

    [refactor] update symbol layer keymap for improved layout and functionality

--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
@@ -115,9 +115,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_SYM] = LAYOUT_elora_hlc(
       _______, _______, _______, _______, _______, _______,                                     _______, _______, _______, _______, _______, _______,
-      KC_GRV ,   KC_1 ,   KC_2 ,   KC_3 ,   KC_4 ,   KC_5 ,                                       KC_6 ,   KC_7 ,   KC_8 ,   KC_9 ,   KC_0 , KC_EQL ,
-     KC_TILD , KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
-     KC_PIPE , KC_BSLS, KC_COLN, KC_SCLN, KC_MINS, KC_LBRC, KC_LCBR, _______, _______, KC_RCBR, KC_RBRC, KC_UNDS, KC_COMM,  KC_DOT, KC_SLSH, KC_QUES,
+      _______, KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC,                                     _______, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS, _______,
+      _______, KC_ASTR, KC_LBRC, KC_LCBR, KC_LPRN, KC_AMPR,                                     KC_PIPE, KC_QUOT, KC_DQUO, KC_GRV , KC_ASTR, _______,
+      _______, _______, KC_RBRC, KC_RCBR, KC_RPRN, KC_CIRC, _______, _______, _______, _______, _______, KC_TILD, _______, _______, KC_BSLS, _______,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
      _______, _______,  _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
