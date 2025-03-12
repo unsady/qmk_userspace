620c9fca3e4be68f73a6f80c1a2799243bbb7dd7
unsady <unsady@gmail.com>
2025-03-12 22:06:09 +0100

    [refactor] update keymap to include new modifiers and add backspace combo

--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
@@ -31,8 +31,11 @@ enum custom_keycodes {
 #define ALT_TAB MT(MOD_LALT, KC_TAB)
 #define NAV_SPC LT(_NAV, KC_SPC)
 #define SYM_SPC LT(_SYM, KC_SPC)
+#define GUI_D MT(MOD_LGUI, KC_D)
+#define GUI_K MT(MOD_LGUI, KC_K)
+#define SFT_F MT(MOD_LSFT, KC_F)
+#define SFT_J MT(MOD_LSFT, KC_J)
 
-#define CW_TOGG MT(MOD_LCTL, KC_CAPS)
 #define RSFT_EQL MT(MOD_RSFT, KC_EQL)
 #define ONE_SFT OSM(MOD_LSFT)
 #define ONE_RSFT OSM(MOD_RSFT)
@@ -71,10 +74,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_elora_hlc(
      KC_EQL  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_MINS,
-     QK_GESC  , KC_Q ,  KC_W  ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     GUI_TAB , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,GUI_QUOT,
+     QK_GESC , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
+     GUI_TAB , KC_A ,  KC_S   , GUI_D  ,  SFT_F ,   KC_G ,                                        KC_H,  SFT_J , GUI_K ,   KC_L ,KC_SCLN,GUI_QUOT,
      ONE_SFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,CW_TOGG,     ADJUST  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,ONE_RSFT,
-                                 MY_LANG, KC_LCTL, ALT_ENT, NAV_SPC, TO(_NAV),   OSL(_SYM),SYM_SPC, KC_RALT, KC_RCTL,FKEYS,
+                                 KC_LCTL,KC_LALT, NAV_SPC,KC_ENT, MY_LANG,      FKEYS, KC_NO, SYM_SPC, KC_RCTL,KC_RALT,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
@@ -255,6 +258,7 @@ const uint16_t PROGMEM sym_combo[] = {KC_J, KC_K, COMBO_END};
 const uint16_t PROGMEM arr_combo[] = {KC_EQL, KC_PLUS, COMBO_END};
 const uint16_t PROGMEM tmp_combo[] = {KC_DLR, KC_PERC, COMBO_END};
 const uint16_t PROGMEM ent_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+const uint16_t PROGMEM bkspc_combo[] = {KC_I, KC_O, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(copy_combo, G(KC_C)),
@@ -268,4 +272,5 @@ combo_t key_combos[] = {
     COMBO(arr_combo, MY_ARR),
     COMBO(tmp_combo, MY_TMP),
     COMBO(ent_combo, KC_ENT),
+    COMBO(bkspc_combo, KC_BSPC),
 };
