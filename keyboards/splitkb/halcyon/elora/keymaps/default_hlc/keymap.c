cdb57359974e03dd7f62f705a848cd9faa381a92
unsady <unsady@gmail.com>
2025-03-18 18:50:00 +0100

    [refactor] update keymap to replace default key placements with placeholders for improved customization

--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/keymap.c
@@ -76,10 +76,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_QWERTY] = LAYOUT_elora_hlc(
-     KC_EQL  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_RBRC,
-     QK_GESC , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_LBRC,
-     GUI_TAB , SRT_A ,  SRT_S  , GUI_D  ,  SFT_F ,   KC_G ,                                        KC_H,  SFT_J , GUI_K ,   SRT_L ,SRT_SCLN,GUI_QUOT,
-     ONE_SFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_BSPC,CW_TOGG,     ADJUST  , KC_MINS, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,ONE_RSFT,
+    _______, KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_RBRC,
+    _______, KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_LBRC,
+    _______, SRT_A ,  SRT_S  , GUI_D  ,  SFT_F ,   KC_G ,                                        KC_H,  SFT_J , GUI_K ,   SRT_L ,SRT_SCLN,GUI_QUOT,
+    _______, KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_BSPC,CW_TOGG,     ADJUST  , KC_MINS, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH,ONE_RSFT,
                                  KC_LCTL,KC_LALT,  NAV_BSPC, NAV_SPC, MY_LANG,      FKEYS,  SYM_SPC, SYM_SPC, KC_RALT,KC_RCTL,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
