857522642bada681cb4a9efebccfd932de1c5fe4
unsady <38117114+unsady@users.noreply.github.com>
2025-09-15 22:22:55 +0200

    Add keymap file for ferris layout

--- keyboards/splitkb/halcyon/elora/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/elora/keymaps/default_hlc/rules.mk
@@ -2,3 +2,7 @@ ENCODER_MAP_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
+# CAPS_WORD_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
+COMBO_ENABLE = yes
+# # CONSOLE_ENABLE = yes
