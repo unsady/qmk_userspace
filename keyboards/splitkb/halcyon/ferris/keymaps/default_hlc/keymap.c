192c7c69e8c63599aa840acd20af4d5f651a34f6
unsady <38117114+unsady@users.noreply.github.com>
2025-09-15 23:16:13 +0200

    refactor: Update keymap layout and improve readability with aliases

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
@@ -7,19 +7,25 @@ enum layers {
     _ADJUST,
 };
 
+// Aliases for readability
+#define QWERTY TO(_QWERTY)
+#define SYM MO(_SYM)
+#define NAV MO(_NAV)
+#define NAV_BSPC LT(_NAV, KC_BSPC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_ferris_hlc(
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,
-        LSFT_T(KC_A), LT(5,KC_S),   LT(1,KC_D),   LT(3,KC_F),   KC_G,         KC_H,         LT(4,KC_J),   LT(2,KC_K),   LT(6,KC_L),   LSFT_T(KC_SCLN),
-        KC_Z,         LCTL_T(KC_X), LALT_T(KC_C), KC_V,         KC_B,         KC_N,         KC_M,         LALT_T(KC_COMM), LCTL_T(KC_DOT), KC_SLSH,
-                                                  KC_P0,        KC_BSPC,      LT(7,KC_SPC), KC_P1,
+        KC_A,         KC_S,         KC_D,         KC_F,         KC_G,         KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,
+        KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,
+                                                  KC_P0,        KC_BSPC,      KC_SPC,       KC_P1,
         KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO
     ),
     [_NAV] = LAYOUT_ferris_hlc(
-        RM_TOGG,      RM_NEXT,      RM_HUEU,      RM_SATU,      RM_VALU,      RM_SPDU,      MS_BTN1,      MS_WHLU,      MS_BTN2,      KC_TRNS,
-        KC_TRNS,      MS_BTN2,      KC_NO,        MS_BTN1,      KC_TRNS,      KC_TRNS,      MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,
-        KC_TRNS,      RM_PREV,      RM_HUED,      RM_SATD,      RM_VALD,      RM_SPDD,      MS_WHLL,      MS_WHLD,      MS_WHLR,      KC_TRNS,
-                                                  KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,
+        QWERTY , _______, _______, G(KC_SPC), _______,KC_PGUP, KC_TAB , KC_UP, KC_BSPC,  KC_ESC ,
+        KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,KC_ENT,
+        G(KC_Z), G(KC_X), G(KC_C), G(KC_V), C(KC_C),QK_REP , KC_TAB ,_______,_______,_______,
+                                                  _______, _______, _______, _______,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
     [_SYM] = LAYOUT_ferris_hlc(
@@ -30,12 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
     [_ADJUST] = LAYOUT_ferris_hlc(
-        KC_TRNS,      KC_TRNS,      KC_COLN,      KC_ESC,       KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_DEL,
-        KC_TRNS,      KC_PERC,      KC_SLSH,      KC_ENT,       KC_TRNS,      DF(1),        KC_LGUI,      KC_TRNS,      KC_TRNS,      KC_TRNS,
-        KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_EXLM,      KC_TRNS,      DF(0),        KC_TRNS,      RALT_T(KC_COMM), RCTL_T(KC_DOT), QK_BOOT,
-                                                  KC_TRNS,      KC_TAB,       KC_NO,        KC_TRNS,
+        RM_TOGG,      RM_NEXT,      RM_HUEU,      RM_SATU,      RM_VALU,      RM_SPDU,      MS_BTN1,      MS_WHLU,      MS_BTN2,      KC_TRNS,
+        KC_TRNS,      MS_BTN2,      KC_NO,        MS_BTN1,      KC_TRNS,      KC_TRNS,      MS_LEFT,      MS_DOWN,      MS_UP,        MS_RGHT,
+        KC_TRNS,      RM_PREV,      RM_HUED,      RM_SATD,      RM_VALD,      RM_SPDD,      MS_WHLL,      MS_WHLD,      MS_WHLR,      KC_TRNS,
+                                                  KC_TRNS,      KC_TRNS,      KC_TRNS,      KC_TRNS,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
-
     )
 };
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
