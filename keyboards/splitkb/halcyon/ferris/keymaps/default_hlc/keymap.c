7ad132e5a80feeaf77a348553b24ac7c3f49d249
unsady <38117114+unsady@users.noreply.github.com>
2025-09-16 23:42:50 +0200

    Add NUM layer definition and update keymap to include NUM key

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
@@ -25,6 +25,7 @@ enum custom_keycodes {
 #define SYM_SPC LT(_SYM, KC_SPC)
 #define ONE_SFT OSM(MOD_LSFT)
 #define SHRT MO(_SHRT)
+#define NUM MO(_NUM)
 #define ADJUST MO(_ADJUST)
 #define WIN LM(_WIN, MOD_LGUI)
 #define ONE_CMD OSM(MOD_LGUI)
@@ -41,14 +42,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QWERTY , _______, _______, G(KC_SPC), _______,KC_PGUP, KC_TAB , KC_UP, KC_BSPC,  KC_ESC ,
         KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, MY_LANG, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,KC_ENT,
         G(KC_Z), G(KC_X), G(KC_C), G(KC_V), C(KC_C),QK_REP , KC_TAB ,_______,_______,_______,
-                                                  _______, _______, WIN, _______,
+                                                  _______, _______,  NUM, WIN,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
     [_SYM] = LAYOUT_ferris_hlc(
       KC_EXLM,  KC_AT , KC_HASH,  KC_DLR, KC_PERC, _______, KC_EQL , KC_PLUS, KC_MINS, KC_UNDS,
       KC_ASTR, KC_LBRC, KC_LCBR, KC_LPRN, KC_AMPR, KC_PIPE, KC_QUOT, KC_DQUO, KC_GRV , KC_ASTR,
       _______, KC_RBRC, KC_RCBR, KC_RPRN, KC_CIRC, _______, KC_TILD, _______, _______, KC_BSLS,
-                                                  _______, _______, _______, _______,
+                                                  _______, NUM, _______, _______,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
     [_ADJUST] = LAYOUT_ferris_hlc(
@@ -71,7 +72,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,      _______,   _______,       _______,     _______,      _______,      _______,      _______,      _______,      _______,
                                                   _______,      _______,      _______,      _______,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
-    )
+    ),
+    [_NUM] = LAYOUT_ferris_hlc(
+        KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_F1 ,  KC_F2 ,  KC_F3 ,  KC_F4 , KC_F5, 
+        KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_F6 ,  KC_F7 ,  KC_F8 ,  KC_F9 , KC_F10,
+        _______,      _______,   _______,       _______,     _______,      KC_F11,  KC_F12,  _______,      _______,      _______, 
+                                                  _______,      _______,      _______,      _______,
+        _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
+    ),
 };
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
