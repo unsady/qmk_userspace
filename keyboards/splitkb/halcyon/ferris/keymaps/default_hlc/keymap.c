c2368f6cbeb1fe7eb801c8fd1d73ae390f7cb95b
unsady <38117114+unsady@users.noreply.github.com>
2025-09-17 15:14:24 +0200

    Update NAV layer to replace A(QK_GESC) with A(KC_GRV)

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO
     ),
     [_NAV] = LAYOUT_ferris_hlc(
-        A(QK_GESC), G(S(KC_N)), _______, G(KC_SPC), _______,KC_PGUP, KC_TAB , KC_UP, KC_BSPC,  KC_ESC ,
+        A(KC_GRV), G(S(KC_N)), _______, G(KC_SPC), _______,KC_PGUP, KC_TAB , KC_UP, KC_BSPC,  KC_ESC ,
         KC_LCTL, KC_LALT, KC_LGUI, KC_LSFT, MY_LANG, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,KC_ENT,
         G(KC_Z), G(KC_X), G(KC_C), G(KC_V), C(KC_C),QK_REP , KC_TAB ,_______,_______,TO(_ADJUST),
                                                   _______, _______,  NUM, WIN,
