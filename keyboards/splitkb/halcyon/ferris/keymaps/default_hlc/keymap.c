99881bb68f7046b9c8b8624bfcf851819c9f3040
unsady <38117114+unsady@users.noreply.github.com>
2026-01-04 12:45:14 +0100

    Rearrange NUM layer keymap for improved layout

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
@@ -72,9 +72,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
     [_NUM] = LAYOUT_ferris_hlc(
-        KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_NO,   KC_ASTR, KC_7,   KC_8,   KC_9,   KC_SLSH,
-        KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_0   , KC_PLUS, KC_4,   KC_5,   KC_6,   KC_MINS,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                KC_NO, KC_0,    KC_1,   KC_2,   KC_3,   KC_DOT,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_ASTR, KC_7,    KC_8,    KC_9,    KC_SLSH,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PLUS, KC_4,    KC_5,    KC_6,    KC_MINS,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,
                                                   KC_NO,KC_BSPC,NUM,KC_0,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
