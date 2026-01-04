0672f405afbbb33a80e33089374baa75fa8d59b3
unsady <38117114+unsady@users.noreply.github.com>
2026-01-04 13:15:38 +0100

    Update NAV layer keymap and enable CAPS_WORD functionality

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NAV] = LAYOUT_ferris_hlc(
         G(KC_SPC), HYPR(KC_W), HYPR(KC_E), HYPR(KC_R), HYPR(KC_T), KC_PGUP, KC_TAB , KC_UP, KC_BSPC,  KC_ESC ,
         ONE_CTL, ONE_ALT, ONE_CMD, ONE_SFT, MY_LANG, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,KC_ENT,
-        G(KC_Z), G(KC_X), G(KC_C), G(KC_V), C(KC_C),G(S(KC_N)), KC_TAB , QK_REP,_______,_______,
+        G(KC_Z), G(KC_X), G(KC_C), G(KC_V), C(KC_C),G(S(KC_N)), QK_REP, QK_REP, QK_CAPS_WORD,_______,
                                                   _______, _______,  NUM, WIN,
         _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______,      _______
     ),
--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/rules.mk
@@ -3,7 +3,7 @@ ENCODER_MAP_ENABLE = yes
 
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
-# CAPS_WORD_ENABLE = yes
+CAPS_WORD_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
 COMBO_ENABLE = yes
 # CONSOLE_ENABLE = yes
