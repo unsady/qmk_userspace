582e5f699432791dd35c73d02e2dfd7310416df7
unsady <38117114+unsady@users.noreply.github.com>
2026-01-04 13:14:04 +0100

    Enable REPEAT_KEY and LEADER functionality in rules.mk

--- keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/rules.mk
+++ keyboards/splitkb/halcyon/ferris/keymaps/default_hlc/rules.mk
@@ -4,7 +4,7 @@ ENCODER_MAP_ENABLE = yes
 # This adds module functionality to your keyboard (files found in users/halcyon_modules)
 USER_NAME := halcyon_modules
 # CAPS_WORD_ENABLE = yes
-# REPEAT_KEY_ENABLE = yes
+REPEAT_KEY_ENABLE = yes
 COMBO_ENABLE = yes
 # CONSOLE_ENABLE = yes
 LEADER_ENABLE = yes
